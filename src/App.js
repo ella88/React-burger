ec78312aff33be8928d4069d49647f2b5e0f0596
Mu Wang <muwang88@gmail.com>
2020-11-05 22:20:10 -0800

    props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
     render() {
         return ( 
         <div className = "App" >
-            <h1> Hi, welcome to REACT! Let's begin! </h1> 
+            <h1 > Hi, welcome to REACT!Let 's begin! </h1>  
+            <p>This is a paragraph.</p>
+            <Person name = "alice" age="25"></Person>
+            <Person name = "Max" age="28">My Hobby is gaming!</Person>
+            <Person name = "Dan" age="32"></Person>
         </div>
         );
+        //return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hi, I\'m a React Div.'))
     }
 }
 
